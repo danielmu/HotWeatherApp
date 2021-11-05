dedff46e4aa0833c06be84c16324d7b6d04bb342
danielmu <gmdannz9000@gmail.com>
2021-11-05 10:39:53 -0700

    Update Extensions.swift

--- DanMuana_HotWeatherApp/DanMuana_HotWeatherApp/Extensions.swift
+++ DanMuana_HotWeatherApp/DanMuana_HotWeatherApp/Extensions.swift
@@ -12,4 +12,21 @@ extension UIColor {
     class func darkBlue() -> UIColor {
         return UIColor(red: 0.116, green: 0.122, blue: 0.589, alpha: 0.9)
     }
+    
+    class func darkRed() -> UIColor {
+        return UIColor(red: 0.2, green: 0.002, blue: 0.002, alpha: 0.9)
+    }
+}
+
+extension UILabel {
+    // TAGS - 34404 | "ERROR Label tag" - 
+    class func initLabelAtPosition(x: Int, y: Int) -> UILabel {
+        let label = UILabel.init(frame: CGRect(x: 0, y: 0, width: 320, height: 20))
+        label.textColor = .darkRed()
+        label.center = CGPoint(x: x, y: y)
+        label.textAlignment = .center
+        
+        label.tag = 34404
+        return label
+    }
 }
